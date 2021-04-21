01151ab1be383e70c801b7f74bea9842f6ad4e40
Gabriel Silva <j.gabriel.888@gmail.com>
2021-04-21 19:31:16 -0400

    separated the date component from the expenseItem

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,9 +1,14 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 export default function ExpenseItem(props) {
 	return (
 		<div className="expense-item">
-			<div>{props.date.toISOString()}</div>
+			<ExpenseDate
+				date={props.date}
+				title={props.title}
+				amount={props.amount}
+			/>
 			<div className="expense-item__description">
 				<h2>{props.title}</h2>
 			</div>
